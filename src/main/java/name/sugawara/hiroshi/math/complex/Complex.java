ea55e63954bb9ef6dc6791bcf07ef5b57f30f9f0
Hiroshi Sugawara <hiroshi@sugawara.name>
2017-10-17 22:57:51 +0900

    First Commit

---
+++ src/main/java/name/sugawara/hiroshi/math/complex/Complex.java
@@ -0,0 +1,61 @@
+/*
+ * 作成日: 2003/10/29
+ */
+package name.sugawara.hiroshi.math.complex;
+
+/**
+ * 複素数.
+ * 
+ * @author Hiroshi Sugawara
+ * @version $Id: Complex.java 109 2010-06-13 04:26:48Z sugawara $
+ * @since 2003/10/29 18:13:38
+ */
+public abstract class Complex extends Number {
+
+  /**
+   * 
+   * @since 2010/04/13　0:15:39
+   */
+  private static final long serialVersionUID = -5652143648697946069L;
+
+  // /**
+  // *
+  // * @since 2003/10/29 18:13:38
+  // */
+  // public Complex() {
+  // super();
+  // }
+  //
+  // /**
+  // * @see java.lang.Number#doubleValue()
+  // * @since 2003/10/29 18:13:38
+  // */
+  // public double doubleValue() {
+  // return 0;
+  // }
+  //
+  // /**
+  // * @see java.lang.Number#floatValue()
+  // * @since 2003/10/29 18:13:38
+  // */
+  // public float floatValue() {
+  // return 0;
+  // }
+  //
+  // /**
+  // * @see java.lang.Number#intValue()
+  // * @since 2003/10/29 18:13:38
+  // */
+  // public int intValue() {
+  // return 0;
+  // }
+  //
+  // /**
+  // * @see java.lang.Number#longValue()
+  // * @since 2003/10/29 18:13:38
+  // */
+  // public long longValue() {
+  // return 0;
+  // }
+
+}
